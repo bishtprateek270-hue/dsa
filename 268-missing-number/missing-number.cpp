555c4df366e14315cb642863cad67bd1146b93ed
prateek singh bisht <bishtprateek270@gmail.com>
2026-08-18 21:00:08 +0530

    Time: 29 ms (5.82%) | Memory: 28 MB (5.14%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        unordered_set<int>s;
+        for(int i : nums){
+            s.insert(i);
+        }
+        for(int i=0;i<=nums.size();i++){
+            if(s.find(i)==s.end()){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
