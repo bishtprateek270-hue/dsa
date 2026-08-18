3592c084bfe57f3a2096f24b8139e95323416dd1
prateek singh bisht <bishtprateek270@gmail.com>
2026-08-18 21:24:03 +0530

    Time: 2 ms (49.90%) | Memory: 9.9 MB (26.23%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()){
+      if(s.length()!=t.length()){
+        return false;
+      }
+      unordered_map<char,int>mp;
+      for (char c:s){
+        mp[c]++;
+      }
+      for(char c : t){
+        mp[c]--;
+      }
+      for(auto x: mp){
+        if(x.second!=0){
             return false;
         }
-        unordered_map<char,int>freq;
-        for(char c: s){
-            freq[c]++;
-        }
-        for(char c : t){
-            freq[c]--;
-        }
-        for(auto x : freq){
-            if(x.second!=0){
-                return false;
-            }
-            
-        }
-        return true;
+      }
+      return true;
     }
 };
